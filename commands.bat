bfc83bd820ae9df0f7f7c2cf91aa1bdd48694877
Blood Moon <163161573+BloodMxxn@users.noreply.github.com>
2024-09-13 22:53:06 -0700

    Update commands.bat

--- commands.bat
+++ commands.bat
@@ -1,4 +1,4 @@
 @ECHO OFF
-CURL -s -o image.jpg https://e1.pxfuel.com/desktop-wallpaper/889/529/desktop-wallpaper-scary-jumpscare.jpg
+CURL -s -o image.jpg https://letsenhance.io/static/03620c83508fc72c6d2b218c7e304ba5/11499/UpscalerAfter.jpg
 START image.jpg
 EXIT
